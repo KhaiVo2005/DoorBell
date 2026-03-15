e7215f32d8159ef6c0576e51e769321450cd0276
KhaiVo2005 <khaivo000@gmail.com>
2026-03-15 19:39:16 +0700

    update delete attribute code in device because its redulant

--- DoorBell.Domain/Entities/Device.cs
+++ DoorBell.Domain/Entities/Device.cs
@@ -12,7 +12,6 @@ namespace DoorBell.Domain.Entities
         public string Name { get; set; }
         public string Type { get; set; }
         public Guid? ParentDeviceId { get; set; }
-        public string Code { get; set; }
         public string ApiKey { get; set; }
         public string Location { get; set; }
         public DateTime CreatedAt { get; set; }
