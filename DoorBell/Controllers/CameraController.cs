92e0b11d8e1ea5718b8c4a5cfd7ae5d12f1f5d54
KhaiVo2005 <khaivo000@gmail.com>
2026-03-24 15:27:30 +0700

    add method check image has people or no

---
+++ DoorBell/Controllers/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DoorBell.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CameraController : ControllerBase
+    {
+        [HttpPost("detect")]
+        public async Task<IActionResult> Detect(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file");
+
+            Console.WriteLine("Received image");
+
+            //var path = Path.Combine("uploads", $"{Guid.NewGuid()}.jpg");
+            //Directory.CreateDirectory("uploads");
+
+            //using (var stream = new FileStream(path, FileMode.Create))
+            //{
+            //    await file.CopyToAsync(stream);
+            //}
+
+            bool hasPeople = new Random().Next(0, 2) == 1;
+
+            return Ok(new
+            {
+                hasPeople = hasPeople
+            });
+        }
+    }
+}
