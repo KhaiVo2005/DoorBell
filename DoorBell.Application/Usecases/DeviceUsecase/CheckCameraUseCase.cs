55d851827e17105ad72cef772a3549855aca0fc2
KhaiVo2005 <khaivo000@gmail.com>
2026-03-31 13:14:29 +0700

    ok

--- DoorBell.Application/Usecases/DeviceUsecase/CheckCameraUseCase.cs
+++ DoorBell.Application/Usecases/DeviceUsecase/CheckCameraUseCase.cs
@@ -10,10 +10,12 @@ namespace DoorBell.Application.Usecases.DeviceUsecase
     public class CheckCameraUseCase
     {
         IDevice _deviceRepository;
+        IDoorBellEvent _doorbellEvent;
 
-        public CheckCameraUseCase(IDevice deviceRepository)
+        public CheckCameraUseCase(IDevice deviceRepository, IDoorBellEvent doorbellEvent)
         {
             _deviceRepository = deviceRepository;
+            _doorbellEvent = doorbellEvent;
         }
 
         public async Task<bool> Execute(string apiKey)
@@ -24,6 +26,21 @@ namespace DoorBell.Application.Usecases.DeviceUsecase
                 return false;
 
             var userId = devices.First().UserId;
+
+            var hasCamera = await _deviceRepository.AnyRootCameraHasPeople(userId);
+
+            if(!hasCamera)
+            {
+                var createEvent = await _doorbellEvent.Create(new Domain.Entities.DoorBellEvent
+                {
+                    Id = Guid.NewGuid(),
+                    DeviceId = devices.First().Id,
+                    EventType = "isringing",
+                    Timestamp = DateTime.UtcNow,
+                    ImageUrl = "",
+                    IsView = false
+                });
+            }    
 
             return await _deviceRepository.AnyRootCameraHasPeople(userId);
         }
--- DoorBell/Controllers/CameraController.cs
+++ DoorBell/Controllers/CameraController.cs
@@ -12,20 +12,14 @@ namespace DoorBell.API.Controllers
         DetectUsecase _detectUsecase;
         DetectPersonUsecase _detectPersonUsecase;
         CheckCameraUseCase _checkPress;
-        Application.Usecases.DoorBellEventUsecase.CreateUsecase _createUsecase;
-        GetByApiKeyUsecase _getDeviceByApiKeyUsecase;
         public CameraController(
             DetectUsecase detectUsecase, 
             DetectPersonUsecase detectPersonUsecase,
-            CheckCameraUseCase checkCameraUseCase,
-            Application.Usecases.DoorBellEventUsecase.CreateUsecase createUsecase,
-            GetByApiKeyUsecase getDeviceByApiKeyUsecase)
+            CheckCameraUseCase checkCameraUseCase)
         {
             _detectUsecase = detectUsecase;
             _detectPersonUsecase = detectPersonUsecase;
             _checkPress = checkCameraUseCase;
-            _createUsecase = createUsecase;
-            _getDeviceByApiKeyUsecase = getDeviceByApiKeyUsecase;
         }
 
         [HttpPost("detect")]
@@ -56,15 +50,6 @@ namespace DoorBell.API.Controllers
         [HttpPost("press")]
         public async Task<bool> Press([FromBody] string apiKey)
         {
-            var device = await _getDeviceByApiKeyUsecase.Execute(apiKey);
-            _createUsecase.Execute(new DoorBell.Application.DTOs.DoorBellEventDTOs.CreateDTO
-            {
-                DeviceId = device.First().Id,
-                EventType = "isringing",
-                Timestamp = DateTime.UtcNow,
-                IsView = false,
-                ImageUrl = null
-            });
             return await _checkPress.Execute(apiKey);
         }
     }
